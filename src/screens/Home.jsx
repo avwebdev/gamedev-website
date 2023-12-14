8b16432f1db138c11e6d57c05d0a311fab6cfc9a
Kethan Vegunta <kethan@vegunta.com>
2023-12-13 23:15:27 -0800

    feat(home): add custom scrollbar and update design

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,41 +1,42 @@
-import { useGlitch } from "react-powerglitch";
+// import { useGlitch } from "react-powerglitch";
 import { Flowbite, Navbar, Footer } from "flowbite-react";
 
 import { TbMail } from "react-icons/tb";
 import { SiDiscord, SiInstagram, SiUnity, SiGodotengine } from "react-icons/si";
 
-
 function Home() {
-  const glitch = useGlitch({
-    "playMode": "always",
-    "createContainers": true,
-    "hideOverflow": false,
-    "timing": {
-      "duration": 2000,
-    },
-    "glitchTimeSpan": false,
-    "shake": {
-      "velocity": 1,
-      "amplitudeX": 0.01,
-      "amplitudeY": 0.01
-    },
-    "slice": {
-      "count": 3,
-      "velocity": 20,
-      "minHeight": 0.01,
-      "maxHeight": 0.3,
-      // "hueRotate": true
-    },
-    "pulse": false
-  });
+  // const glitch = useGlitch({
+  //   "playMode": "always",
+  //   "createContainers": true,
+  //   "hideOverflow": false,
+  //   "timing": {
+  //     "duration": 2000,
+  //   },
+  //   "glitchTimeSpan": false,
+  //   "shake": {
+  //     "velocity": 1,
+  //     "amplitudeX": 0.01,
+  //     "amplitudeY": 0.01
+  //   },
+  //   "slice": {
+  //     "count": 3,
+  //     "velocity": 20,
+  //     "minHeight": 0.01,
+  //     "maxHeight": 0.3,
+  //   },
+  //   "pulse": false
+  // });
 
   const customFlowbiteTheme = {
     navbar: {
       root: {
-        base: "bg-[#011223] px-2 py-3 sm:px-3 fixed top-0 inset-x-0 shadow-xl",
+        base: "backdrop-blur bg-gradient-to-b from-[#7da279] to-[#7da2794A] px-2 py-3 sm:px-3 fixed top-0 inset-x-0 shadow-xl",
+      },
+      collapse: {
+        list: "mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-5 md:text-sm md:font-medium"
       },
       link: {
-        base: "text-[#e9ffb2] text-[1.05rem] rounded-full bg-[#397b6c] py-2 px-5",
+        base: "font-semibold text-[#e9ffb2] text-[1.25rem] rounded-full px-5 py-2 inline-block transition duration-300 hover:scale-[1.1] hover:bg-[#55854a]",
         active: {
           on: "",
           off: ""
@@ -82,17 +83,21 @@ function Home() {
           </Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
-      <div id="main" className="main center bg-gradient-to-b from-[#1e667b] to-[#031622] text-[#e7fdb0]">
-          <p className="text-6xl">AV Game Dev</p>
-          <hr className="w-96 h-2 mx-auto border-0 rounded bg-[#e9ffb2]"></hr>
-          <p className="text-2xl">Building the Future of Gaming</p>
-        </div>
+      <div id="main" className="main center bg-[#263223] text-[#e7fdb0]">
+        <p className="text-6xl">AV Game Dev</p>
+        <hr className="w-96 h-1.5 mx-auto border-0 rounded bg-[#e9ffb2]"></hr>
+        <p className="text-2xl">Building the Future of Gaming</p>
+      </div>
       <div id="about" className="partition text-[#e9ffb2] bg-gradient-to-b from-[#9bae75] to-[#449869]">
-        <p className="text-4.5xl">Welcome to AV Game Dev</p>
-        <hr className="w-132 h-1 mt-2 mb-5 mx-auto border-0 rounded bg-[#e9ffb2]"></hr>
-        <p className="text-lg w-176 text-center">In this club, we teach our club members how to develop their own games in game engines used in the industry, such as Unity and Godot, and allow them to collaborate with other people to develop their own projects.</p>
+        <p className="text-4.5xl">About the Club</p>
+        <hr className="w-96 h-1 mt-2 mb-5 mx-auto border-0 rounded bg-[#e9ffb2]"></hr>
+        <p className="text-lg w-176 text-center">In this club, we teach our club members how to develop games with game engines used in the industry, such as Unity and Godot, and allow them to collaborate with other people to develop their own projects.</p>
       </div>
       <div id="join" className="partition text-[#e9ffb2] bg-[#449869]">
+        <div className="float-right">
+          <p className="text-4.5xl">Join the Club!</p>
+          <hr className="w-72 h-1.5 mx-auto border-0 rounded bg-[#e9ffb2]"></hr>
+        </div>
       </div>
       <Footer className="!bg-[#2f415b]">
         <div className="w-full bg-gray-700 px-4 py-6 center">
